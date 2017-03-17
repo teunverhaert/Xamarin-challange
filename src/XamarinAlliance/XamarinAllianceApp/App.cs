074e215952664509f1e0efe59fd2ac72c66b761c
Teun Verhaert <teun.verhaert@student.ap.be>
2017-03-17 19:44:44 +0100

    Eerste versie

---
+++ src/XamarinAlliance/XamarinAllianceApp/App.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+
+using Xamarin.Forms;
+using XamarinAllianceApp.Models;
+using XamarinAllianceApp.Views;
+
+namespace XamarinAllianceApp
+{
+	public class App : Application
+	{
+		public App ()
+		{
+            // The root page of your application
+            MainPage = new NavigationPage(new CharacterListPage());
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
